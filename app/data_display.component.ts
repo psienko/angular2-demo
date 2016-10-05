543d3af4e19d33084a0e20aa8eee2a4815638258
Przemek <sienkowskip@gmail.com>
2016-10-05 10:00:46 +0200

    Data display

---
+++ app/data_display.component.ts
@@ -0,0 +1,42 @@
+import {Component, View} from "angular2/core"
+
+@Component({
+    selector: 'data-display'
+})
+
+@View({
+  template: `
+    <h2>Showing data using component properties with interpolation</h2>
+    <h3>Player Name:{{player}}</h3>
+    <h3>He is famous in: {{sport}}</h3><br>
+
+    <h2>Showing data using constructor or variable initialization</h2>
+    <h3>India capital is: {{capital}}</h3><br>
+
+    <h2>Showing data using array property with NgFor</h2>
+    <h3>My favorite fruit is: {{myfruit}}</h3>
+    <p>List of Fruits:</p>
+    <ul>
+      <li *ngFor="#fruit of fruits">
+        {{ fruit }}
+      </li>
+    </ul>
+  `
+})
+
+export class DataDisplayComponent{
+  player : "Ja";
+  sport :  string = 'Football';
+
+  capital: string;
+
+  constructor() {
+    this.capital = 'New Delphi';
+    this.init();
+  }
+
+  fruits = ['Apple 2', 'Orange', 'Banana', 'Mango', 'Grapes'];
+  myfruit = this.fruits[1];
+  init() {this.fruits.push("smth");}
+
+}
---
+++ app/data_display_main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from "angular2/platform/browser"
+import {DataDisplayComponent} from "./data_display.component"
+
+bootstrap(DataDisplayComponent)
